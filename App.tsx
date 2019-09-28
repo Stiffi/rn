95bb7b432b50a9249ca7c511939296d48842d440
Ilya Guskov <ilyaguskoff@gmail.com>
2019-09-29 03:23:13 +0400

    Added lint and prettier

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import {Button, TextInput, View} from 'react-native';
+import { Button, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={{paddingTop: 50, paddingLeft: 20, paddingRight: 20}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between',
-            alignItems: 'center'}}>
-        <TextInput placeholder={"Course goal"}
-                   style={{
-                     borderColor: '#000000',
-                     borderWidth: 1,
-                     padding: 10,
-                   }}/>
-        <Button title={"Add"} onPress={() => {
-}}/>
-      </View>
-    </View>
-  );
+    return (
+        <View style={{ paddingTop: 50, paddingLeft: 20, paddingRight: 20 }}>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                }}
+            >
+                <TextInput
+                    placeholder="Course goal"
+                    style={{
+                        borderColor: '#000000',
+                        borderWidth: 1,
+                        padding: 10
+                    }}
+                />
+                <Button title="Add" onPress={() => {}} />
+            </View>
+        </View>
+    );
 }
