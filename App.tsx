f09d6eca1afd052c8b2f5052508e0efbee3099e8
Ilya Guskov <ilyaguskoff@gmail.com>
2019-10-03 23:33:02 +0400

    Restart project

--- App.tsx
+++ App.tsx
@@ -1,54 +1,19 @@
-import React, { useState } from 'react';
-import {
-    StyleSheet,
-    Text,
-    Button,
-    TextInput,
-    View,
-    FlatList
-} from 'react-native';
-import GoalItem from './components/GoalItem/GoalItem';
-import GoalInput from './components/GoalInput/GoalInput';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const [courseGoals, setCourseGoals] = useState([]);
-    const [isAddMode, setIsAddMode] = useState(false);
-
-    const addGoalHandler = (enteredGoal: string) => {
-        setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
-        setIsAddMode(false);
-    };
-
-    const removeGoalHandler = index => {
-        setCourseGoals(currentGoals =>
-            currentGoals.filter((goal, goalIndex) => goalIndex !== index)
-        );
-    };
-
-    const cancelGoalHandler = () => {
-        setIsAddMode(false);
-    };
-
-    return (
-        <View style={styles.screen}>
-            <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
-            <GoalInput onCancel={cancelGoalHandler} visible={isAddMode} onAddGoal={addGoalHandler} />
-            <FlatList
-                data={courseGoals}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={listItem => (
-                    <GoalItem
-                        onDelete={() => removeGoalHandler(listItem.index)}
-                        title={listItem.item}
-                    />
-                )}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        padding: 50
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
